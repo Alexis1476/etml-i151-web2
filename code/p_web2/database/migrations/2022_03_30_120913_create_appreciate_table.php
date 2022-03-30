eb6e1ba085ef1184fc006d5f1bc1af9a011a45c2
Alexis1476 <halexisr20@hotmail.com>
2022-03-30 15:12:21 +0200

    Création migration create_appreciate_table

---
+++ code/p_web2/database/migrations/2022_03_30_120913_create_appreciate_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('t_appreciate', function (Blueprint $table) {
+            $table->primary(['idBook','idUser']);
+            $table->double('appNote');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('t_appreciate');
+    }
+};
