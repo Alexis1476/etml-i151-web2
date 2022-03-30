ac4c1e85caac96149942edda2c156fda1c055198
dieperid <david.dieperink@ik.me>
2022-03-30 15:03:49 +0200

    Creation de tout les models

---
+++ code/p_web2/app/Models/CategoryModel.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CategoryModel extends Model
+{
+    /**
+     * Fonction qui créer la relation entre la table t_book et la table t_category
+     * Une catégorie possède plusieurs livre
+     * @return 
+    */
+    public function books(){
+        return $this->hasMany(BookModel::class);
+    }
+
+    // Tableau pour les colonnes de la table qui sont remplissables
+    protected $fillable = [
+        'idCategory',
+        'catName'
+    ];
+
+    use HasFactory;
+}
---
+++ code/p_web2/app/Models/EditorModel.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class EditorModel extends Model
+{
+    /**
+     * Fonction qui créer la relation entre la table t_book et la table t_editor
+     * @return 
+    */
+    public function books(){
+        return $this->hasMany(BookModel::class);
+    }
+
+    // Tableau pour les colonnes de la table qui sont remplissables
+    protected $fillable = [
+        'idEditor',
+        'ediName'
+    ];
+
+    use HasFactory;
+}
