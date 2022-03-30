d3d1f416533dabb57a0a5ce4f11a6a5e774b8f6c
Alexis1476 <halexisr20@hotmail.com>
2022-03-30 13:40:22 +0200

    Création migragion create_author_table

---
+++ code/p_web2/database/migrations/2022_03_30_113545_create_author_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('t_author', function (Blueprint $table) {
+            $table->increments('idAuthor');
+            $table->string('autFirstName',60);
+            $table->string('autLastName',60);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('t_author');
+    }
+};
