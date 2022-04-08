0f8bb726a3c382c41e9e2a93ca6afc2bf906fc4a
dieperid <david.dieperink@ik.me>
2022-04-08 15:26:26 +0200

    Book card

--- code/p_web2/resources/views/partials/book-card.blade.php
+++ code/p_web2/resources/views/partials/book-card.blade.php
@@ -1,14 +1,12 @@
-<div class="w-2/6 p-20 pt-0">
-    <div>
-        <h2 class="text-xl font-bold"><a href="#">{{$title}}</a></h2>
-        <div class="grid grid-cols-2 grid-flow-row rounded border-2 border-blue-500 px-1 py-1">
-            <div class="row-start-1">
-                <p class="text-clip overflow-hidden footer-color">{{$author}}</p>
-                <a href="#" class="footer-color text-clip overflow-hidden">{{$user}}</a>
-            </div>
-            <div class="row-start-2"></div>
-            <img src="img/{{$img}}"
-                 class="col-start-2 row-span-2 min-w-3/5 min-h-5/5 justify-self-end rounded border-2 border-blue-500 mt-1 mr-1 mb-1">
-        </div>
+<div class="max-w-md mx-auto mb-10 bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+    <div class="md:flex">
+      <div class="md:shrink-0">
+        <img class="md:w-32 lg:w-48 h-48 w-full object-cover md:h-full md:w-48" src="img/{{$img}}" alt="">
+      </div>
+      <div class="p-8 w-64">
+        <div class="uppercase tracking-wide text-sm text-blue-500 font-semibold hover:underline"><a href="#">{{$title}}</a></div>
+        <p class="block mt-1 text-20 leading-tight font-medium text-black">{{$author}}</p>
+        <a href="#" class="hover:underline">{{$user}}</a>
+      </div>
     </div>
-</div>
+  </div>
