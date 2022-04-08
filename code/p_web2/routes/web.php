d09126b44d98434e08852f2f455ebf785ae25b3b
Alexis1476 <halexisr20@hotmail.com>
2022-04-08 14:48:27 +0200

    Création controllers book et user

--- code/p_web2/routes/web.php
+++ code/p_web2/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
