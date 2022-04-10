40eb3c749eba44c16deb0522c37eec45d73672b2
Alexis1476 <halexisr20@hotmail.com>
2022-04-10 17:35:41 +0200

    Création vue partial form-login

--- code/p_web2/resources/views/partials/form-login.blade.php
+++ code/p_web2/resources/views/partials/form-login.blade.php
@@ -1,7 +1,7 @@
-<h1 class="text-2xl font-bold text-center">Login</h1>
-<div class="flex items-center justify-center content-center h-full">
+<div class="flex items-center justify-center content-center">
     <form action="/userAdd" method="post" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-        {{ csrf_field() }}
+        <h1 class="text-2xl font-bold text-center mb-5">Login</h1>
+        @csrf
         @include('partials.form-input', ['nameItem'=>'user', 'text'=>'Username', 'type'=>'text'])
         @include('partials.form-input', ['nameItem'=>'password', 'text'=>'Password', 'type'=>'password'])
         <div class="mt-1 text-sm text-gray-500 dark:text-gray-300">
