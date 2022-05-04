54e4d2dae19735c457e35d83b5ee19643866bdff
Alexis1476 <halexisr20@hotmail.com>
2022-05-04 15:43:26 +0200

    Ajout gestion des erreurs dans les champs (Bordrure rouge + affichage message d'erreur)

--- code/p_web2/resources/views/partials/form-login.blade.php
+++ code/p_web2/resources/views/partials/form-login.blade.php
@@ -3,8 +3,8 @@
     <form action="/userAdd" method="post" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
         <h1 class="text-2xl font-bold text-center mb-5">Login</h1>
         @csrf
-        @include('partials.form-input', ['nameItem'=>'user', 'text'=>'Username', 'type'=>'text'])
-        @include('partials.form-input', ['nameItem'=>'password', 'text'=>'Password', 'type'=>'password'])
+        @include('partials.form-input', ['nameItem'=>'userLogin', 'text'=>'Username', 'type'=>'text'])
+        @include('partials.form-input', ['nameItem'=>'passwordLogin', 'text'=>'Password', 'type'=>'password'])
         <div class="mt-1 text-sm text-gray-500 dark:text-gray-300">
             <div class="flex items-center justify-between">
                 <button
